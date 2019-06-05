52d4d0e38cf9e38d8ed4fa1af5e0d8c02d92536d
Faiz Mokhtar <mfmokhtar@gmail.com>
2019-06-05 23:39:44 +0800

    Add more hammerspoon script

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,7 @@
 -- Variables
 
 local hyper = {"ctrl", "alt", "cmd"}
+local power = {"ctrl", "cmd"}
 
 -- Spoons
 
@@ -21,5 +22,56 @@ spoon.MiroWindowsManager:bindHotkeys({
 -- Avoid paste blocking
 
 hs.hotkey.bind(hyper, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+-- Listen to wifi events
+
+wifiwatcher = hs.wifi.watcher.new(function ()
+  net = hs.wifi.currentNetwork()
+  if net==nil then
+      hs.notify.show("You lost Wi-Fi connection","","","")
+  else
+      hs.notify.show("Connected to Wi-Fi network","",net,"")
+  end
+end)
+wifiwatcher:start()
+
+-- Quick open applications
+
+function open(name)
+  return function()
+      hs.application.launchOrFocus(name)
+      if name == 'Finder' then
+          hs.appfinder.appFromName(name):activate()
+      end
+  end
+end
+
+hs.hotkey.bind(power, "X", open("Xcode"))
+hs.hotkey.bind(power, "I", open("iTerm"))
+
+-- Quick switch Chrome users
+
+function chrome_switch_to(user)
+  return function()
+      hs.application.launchOrFocus("Google Chrome")
+      local chrome = hs.appfinder.appFromName("Google Chrome")
+      local str_menu_item
+      if user == "Incognito" then
+          str_menu_item = {"File", "New Incognito Window"}
+      else
+          str_menu_item = {"People", user}
+      end
+      local menu_item = chrome:findMenuItem(str_menu_item)
+      if (menu_item) then
+          chrome:selectMenuItem(str_menu_item)
+      end
+  end
+end
+
+hs.hotkey.bind(power, "1", chrome_switch_to("Personal"))
+hs.hotkey.bind(power, "2", chrome_switch_to("Work"))
+hs.hotkey.bind(power, "3", chrome_switch_to("Incognito"))
+
+-- Autoreload configurations
 
 spoon.ReloadConfiguration:start()
